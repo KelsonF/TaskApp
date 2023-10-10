75f51fb7e3de01c9af9f687697fa812ba4f78ff5
Kelson Eduardo <kfeduardo2003@gmail.com>
2023-10-10 00:21:27 -0300

    feat: new pages added

---
+++ src/pages/ErrorPage/index.tsx
@@ -0,0 +1,8 @@
+export function ErrorPage(){
+    return (
+        <main>
+            <h2>Error, the page you've been searching for was not found</h2>
+            <h3>Please try again</h3>
+        </main>
+    )
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,12 +2,16 @@ import { useRoutes } from "react-router-dom";
 import { TaskPage } from "../pages/TaskPage";
 import { HomePage } from "../pages/HomePage";
 import { AboutPage } from "../pages/AboutPage";
+import { LoginPage } from "../pages/LoginPage";
+import { ErrorPage } from "../pages/ErrorPage";
 
 export function Route() {
   const element = useRoutes([
     { path: "/", element: <HomePage /> },
     { path: "/tasks", element: <TaskPage /> },
     { path: "/about", element: <AboutPage /> },
+    { path: "/login", element: <LoginPage /> },
+    {path: "*", element: <ErrorPage />}
   ]);
 
   return element;
